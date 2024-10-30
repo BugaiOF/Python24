ea9d23064bd39c353070b38e41bb2ebb6ea6aa5d
Gustavo Da Silva Silveira <112825271+BugaiOF@users.noreply.github.com>
2024-10-30 02:12:09 -0300

    lets go

---
+++ Python 2024 agora vai/Main23.py
@@ -0,0 +1,8 @@
+def cliente1 (nome):
+    print (f'Olá {nome}')
+
+def cliente2 (nome):
+    return (f'Olá {nome}')
+
+cliente2('João')
+cliente1('Maria')
---
+++ Python 2024 agora vai/Main25.py
@@ -0,0 +1,7 @@
+def agencia(**car_infos):
+    return car_infos
+
+
+print(agencia(Model ='gol', color = 'Roxo' , engine = 1.6))
+print(agencia(Model ='Monza', color = 'Red' , engine = 1.8))
+print(agencia(Model ='Vectra', color = 'Prata' , engine = 2.0))
