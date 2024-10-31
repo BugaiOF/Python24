5bf2857b8400f18ffad2a49c1e286d29a60123f9
Gustavo Da Silva Silveira <112825271+BugaiOF@users.noreply.github.com>
2024-10-31 01:38:54 -0300

    keep going

---
+++ Python 2024 agora vai/Main26.py
@@ -0,0 +1,7 @@
+import math
+
+print (math.ceil(4.9))
+
+fatorial = 4 * 3 * 2 * 1 
+
+print (fatorial)
---
+++ Python 2024 agora vai/Main27.py
@@ -0,0 +1,9 @@
+city1 ='rio de janeiro'
+city2 ='porto alegre'
+city3 ='sp'
+
+
+citys = ['Rio de janeiro', 'Porto alegre', 'Fortaleza']
+
+
+print(citys)
