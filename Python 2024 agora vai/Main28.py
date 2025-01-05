ee801387d912f209799b896ca8233a6cdb9773fb
Gustavo Da Silva Silveira <112825271+BugaiOF@users.noreply.github.com>
2025-01-05 20:16:43 -0300

    2025

---
+++ Python 2024 agora vai/Main28.py
@@ -0,0 +1,13 @@
+city1 ='rio de janeiro'
+city2 ='porto alegre'
+city3 ='sp'
+
+
+citys = ['Rio de janeiro', 'Porto alegre', 'Fortaleza', 'Goiania']
+
+""" citys.append('Santa catarina') """
+""" citys.remove('Rio de janeiro') """
+""" citys.insert(0, 'minas') """
+""" citys.pop(1) """
+citys.sort()
+print(citys)
